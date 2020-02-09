d9f091035f521613a2e982eb041a791810c8b75b
annoyinganime <34567203+annoyinganime@users.noreply.github.com>
2020-02-09 15:24:59 +0300

    Add files via upload

--- ActiveSuspension/ActiveSuspension.ino
+++ ActiveSuspension/ActiveSuspension.ino
@@ -5,13 +5,18 @@
 #define SERVO_MIN 45
 #define SERVO_MAX 135
 #define SERVO_MID 96
+#define X_CAL 0.0
+#define Y_CAL -0.3
+#define Z_CAL 0.2
+#define PITCH -acz
+#define ROLL acy
 
 int d[4]; //for storing angles in "degrees"
 Servo fl, fr, rl, rr; //front-left, front-right, rear-left, rear-right
 void setup() {
   Wire.begin();
   w(0x6b, 0x00); //disable mpu6050 sleep
-  w(0x1c, 0b00001000); //also change gc if changing this
+  w(0x1c, 0b00001000); //maximum accelerometer resolution, also change value in gc if changing this
   w(0x1a, 0b00000110); //filter setup
   
   Serial.begin(115200);
@@ -26,11 +31,8 @@ void setup() {
   fl.attach(5);
  
   rr.write(SERVO_MID);
-  delay(200);
   fr.write(SERVO_MID);
-  delay(200);
   fl.write(SERVO_MID);
-  delay(200);
   rl.write(SERVO_MID);
   delay(500);
   for (int i = 0; i<4; i++){
@@ -40,20 +42,21 @@ void setup() {
 double acx, acy, acz; 
 
 void loop() {
-  // 3b- x, 3d- y, 3f- z (probably unneeded)      rr, fr, rl, fl
-  acx = gc(0x3b); //right-left, right-positive
-  acy = gc(0x3d)+0.3; //front-rear, front-positive
-  acz = gc(0x3f)-0.2;
+  // 3b- x, 3d- y, 3f- z       rr, fr, rl, fl
+  acx = gc(0x3b)-X_CAL; 
+  acy = gc(0x3d)-Y_CAL; 
+  acz = gc(0x3f)-Z_CAL;
+  
   Serial.print(acx);
   Serial.print("   ");
   Serial.print(acy);
   Serial.print("   ");
   Serial.print(acz);
   Serial.println();/**/
-  d[0]=SERVO_MID+acy*SCALE+acz*SCALE;
-  d[1]=SERVO_MID-acy*SCALE+acz*SCALE;
-  d[2]=SERVO_MID+acy*SCALE-acz*SCALE;
-  d[3]=SERVO_MID-acy*SCALE-acz*SCALE;
+  d[0] = SERVO_MID + (-PITCH+ROLL)*SCALE; //rr
+  d[1] = SERVO_MID + (-PITCH-ROLL)*SCALE; //fr
+  d[2] = SERVO_MID + (PITCH+ROLL)*SCALE;  //rl
+  d[3] = SERVO_MID + (PITCH-ROLL)*SCALE;  //fl
   wa(d);
   delay(10);
   
@@ -86,7 +89,7 @@ int8_t r(uint8_t address){ //read one byte
   }
 
 double gc(uint8_t address) { //read and convert to newtons
-  return r2(address)/819.2;
+  return r2(address)/819.2; 
   }
 
 void wa(int d[]){ // write angles to servos
